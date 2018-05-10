c08c55e5ff57b7a353804372449a341dc87581ce
yubinhong <yubinhong@miguantech.com>
2018-05-10 09:56:16 +0800

    【优化】功能

---
+++ web/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+import json
+from web import utils
+import requests
+from backend import analyse
+def index(request):
+    if request.method=='GET':
+        API="http://192.168.100.3:9200/_cat/indices?pretty"
+        req=requests.get(API)
+        index_list=req.text.strip('\n').split('\n')
+        tongji_list=[x.split()[2] for x in index_list]
+        return render(request,'index.html',{'tongji_list':tongji_list})
+    else:
+        start_time=request.POST.get('analyse_at_from')
+        stop_time=request.POST.get('analyse_at_to')
+        index=request.POST.get('index')
+        result=analyse.analyse(start_time,stop_time,index)
+        return HttpResponse(result)
